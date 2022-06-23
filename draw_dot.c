8f5c04e24743b8caadc14c2a94b3aa19c7d71985
matt <05wsnmli@gmail.com>
2022-06-23 18:48:46 +0100

    updted draw dot program

--- draw_dot.c
+++ draw_dot.c
@@ -1,3 +1,4 @@
+//  g++ draw_dot.c -lSDL2 && ./a.out 
 #include <SDL2/SDL.h>
 
 int main() {
@@ -8,8 +9,16 @@ int main() {
     SDL_Renderer* renderer = SDL_CreateRenderer(
                            window, -1, SDL_RENDERER_ACCELERATED);
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-    SDL_RenderClear(renderer);
+    SDL_Event event;
+
+    while(event.type != SDL_QUIT) {
+        SDL_PollEvent(&event);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        SDL_RenderClear(renderer);
+
+        
+        SDL_RenderPresent(renderer);
+    }
 
 
     // clean up
